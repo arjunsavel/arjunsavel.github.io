88f83e6cd2f2720d6227347f98ae6872c7e0bbc3
Arjun Savel <arjunsavel@Arjuns-MacBook-Air.local>
2019-05-22 00:36:27 -0700

    Now adding pages?

--- freeze.py
+++ freeze.py
@@ -1,4 +1,4 @@
-import main
+from project import main
 
 if __name__ == '__main__':
     main.freezer.freeze()
--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, url_for, redirect, request
 from app import app, pages
 
 @app.route('/')
@@ -9,9 +9,15 @@ def home():
         key=lambda page: page.meta['date'])
     return render_template('index.html', pages=sorted_posts)
 
-@app.route('/<path:path>/')
-def page(path):
+@app.route('/bacon', methods=['GET', 'POST'])
+def page(path='bacon'):
     # `path` is the filename of a page, without the file extension
     # e.g. "first-post"
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('index'))
     page = pages.get_or_404(path)
     return render_template('page.html', page=page)
