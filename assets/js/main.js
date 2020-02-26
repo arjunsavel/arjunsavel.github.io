30606727935d7457c3ee86d115dfe0e76ad065c9
Arjun Savel <asavel@berkeley.edu>
2020-02-26 11:24:54 -0800

    changing javascript load

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,28 +3,7 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-import anime from 'anime/anime.es.js';
-
-const anime = require('anime');
-
-// Wrap every letter in a span
-var textWrapper = document.querySelector('.ml3');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml3 .letter',
-    opacity: [0,1],
-    easing: "easeInOutQuad",
-    duration: 2250,
-    delay: (el, i) => 150 * (i+1)
-  }).add({
-    targets: '.ml3',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
+import anime from 'animejs/lib/anime.es.js';
 
 (function($) {
 
